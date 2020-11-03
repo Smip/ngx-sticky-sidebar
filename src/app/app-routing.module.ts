06407a78e5ba85fa2011c023c6ea74b13ef83cbd
Lukas Brezina <lukas.brezina@coex.cz>
2020-11-03 10:48:49 +0100

    add SSR to example app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,8 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     anchorScrolling: 'enabled',
-  })],
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
